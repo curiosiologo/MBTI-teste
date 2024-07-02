943e8f53177eda1cf369e562fdd3d22ea5df13e8
curiosiologo <curiosiologo@gmail.com>
2024-07-02 12:32:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template_string(open('index.html').read())
+    return render_template('index.html')
 
 @app.route('/resultado', methods=['POST'])
 def resultado():
     nome = request.form['nome']
     idade = request.form['idade']
     genero = request.form['genero']
-    
+
     extroversao_introversao = request.form['extroversao_introversao']
     sensacao_intuicao = request.form['sensacao_intuicao']
     pensamento_sentimento = request.form['pensamento_sentimento']
     julgamento_percepcao = request.form['julgamento_percepcao']
-    
+
     mbti = extroversao_introversao + sensacao_intuicao + pensamento_sentimento + julgamento_percepcao
-    
+
     return f"""
     <h1>Resultado do MBTI</h1>
     <p>Nome: {nome}</p>
@@ -26,6 +26,3 @@ def resultado():
     <p>Gênero: {genero}</p>
     <p>Seu tipo MBTI é: {mbti}</p>
     """
-
-if __name__ == '__main__':
-    app.run(debug=True)
